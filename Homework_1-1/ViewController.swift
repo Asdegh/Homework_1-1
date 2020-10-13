5c4380278a6686f85ec094e5ff54f5a72db321f1
Sarhojun <serge_anisimoff@mail.ru>
2020-10-13 13:33:40 +0300

    Initial Commit

---
+++ Homework_1-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_1-1
+//
+//  Created by Asdegh on 13.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
